de3f40d5b437057544d3e7ea84c230e5fd4fa9e1
Nicolas Descreux <nicolasdescreux@hotmail.fr>
2018-11-19 12:59:28 +0100

    stupid coaching

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get '/ask', to: 'question#ask'
+  get '/answer', to: 'question#answer'
+  post '/answer', to: 'question#answer'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
